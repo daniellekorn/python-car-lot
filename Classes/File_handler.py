9f15595c391416492d43c8cd6791f0f86d1d235b
Danielle Korn <daniella.korn@gmail.com>
2020-04-19 14:22:13 +0300

    load csv file func complete

--- Classes/File_handler.py
+++ Classes/File_handler.py
@@ -1,11 +1,26 @@
-FileHandler:
+from csv import DictReader
+
+
+class FileHandler:
     def __init__(self):
         pass
 
     @staticmethod
     def load_from_csv(file_name):
-        pass
+        try:
+            with open(file_name) as file:
+                csv_reader = DictReader(file)
+                for row in csv_reader:
+                    print(row)
+        except FileNotFoundError:
+            print("FileNotFoundError: No such file exists.")
+        except OSError:
+            print("OSError: Bad file descriptor. Type must be string.")
 
     @staticmethod
     def append_to_csv(file_name,data):
         pass
+
+
+# Test
+FileHandler.load_from_csv("/Users/daniellekorn/PycharmProjects/car_lot/CSV/User")
