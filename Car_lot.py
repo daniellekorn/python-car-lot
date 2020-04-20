15c70186ec7165227a6d88c719766b3a755fc44e
Danielle Korn <daniella.korn@gmail.com>
2020-04-20 23:57:38 +0300

    ex3 functional

--- Car_lot.py
+++ Car_lot.py
@@ -18,20 +18,15 @@ class CarLot:
         pass
 
     def update_salary_by_name(self, employee_salary, name):
-        file_data = self.user_handler.get_data()
-        print(file_data)
+        file_data = self.user_handler.load_from_csv()
+        all_employees = []
         for row in file_data:
-            try:
-                updated_data = []
-                if row[2] == name or f"{row[1]} {row[2]}" != name:
-                    updated_data.append(row)
-                else:
-                    employee_raise = [item if index != 5 else employee_salary for index, item in enumerate(row)]
-                    updated_data.append(employee_raise)
-                    self.user_handler.write_to_csv(updated_data)
-                return True
-            except IndexError as e:
-                print("IndexError: " + str(e))
+            if row[2] == name or f"{row[1]} {row[2]}" == name:
+                new_data = [item if index != 5 else employee_salary for index, item in enumerate(row)]
+                all_employees.append(new_data)
+            else:
+                all_employees.append(row)
+        self.user_handler.write_to_csv(all_employees)
 
 
 
@@ -39,5 +34,4 @@ class CarLot:
 # CarLot.add_to_fleet("/Users/daniellekorn/Downloads/mock.csv")
 lot = CarLot()
 print(lot.update_salary_by_name(56, 'sam'))
-print(lot.update_salary_by_name(54, 'Capnerhurst'))
-
+print(lot.update_salary_by_name(54, 'Dorgon'))
