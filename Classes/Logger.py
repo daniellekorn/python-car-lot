70b38adaa74bcd397d1dc97a9beb6675c2934afb
Danielle Korn <daniella.korn@gmail.com>
2020-04-20 12:09:33 +0300

    new check_password_len function on User

--- Classes/Logger.py
+++ Classes/Logger.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 from logging.handlers import TimedRotatingFileHandler
 
 
@@ -10,7 +11,7 @@ class Logger:
         try:
             f = open(self._path, "a")
         except Exception as e:
-            print("Something went wrong: " + str(e))
+            print(str(e))
         else:
             x = datetime.datetime.now()
             f.write(x.strftime("%d/%m/%y %H:%M:%S ") + msg + "\n")
@@ -20,3 +21,29 @@ messages = Logger("/Users/daniellekorn/PycharmProjects/car_lot/msg_log.log")
 messages.add_to_log("test")
 messages.add_to_log("test two")
 messages.add_to_log("test three")
+
+
+# import datetime
+# import logging
+# from logging.handlers import TimedRotatingFileHandler
+#
+#
+# class Logger:
+#     def __init__(self, path):
+#         self._path = path
+#
+#     def add_to_log(self, msg):
+#         logger = logging.getLogger(__name__)
+#         logger.setLevel(logging.INFO)
+#
+#         try:
+#             handler = TimedRotatingFileHandler(self._path, when="D", backupCount=24)
+#             logger.addHandler(handler)
+#         except Exception as e:
+#             print("Something went wrong: " + str(e))
+#         else:
+#             x = datetime.datetime.now()
+#             logger.info(x.strftime("%d/%m/%y %H:%M:%S ") + msg + "\n")
+#
+#             logger = logging.getLogger(__name__)
+#             logger.setLevel(logging.INFO)
--- Classes/User.py
+++ Classes/User.py
@@ -19,12 +19,20 @@ class User:
     @password.setter
     def password(self, new_password):
         if isinstance(new_password, str):
-            if (len(new_password) > 4) and (len(new_password) < 12):
+            if self.__valid_password_length(new_password):
                 self._password = new_password
             else:
                 raise ValueError("Password must be between 4-12 characters")
         else:
             raise TypeError("Password must be type string")
+
+    @staticmethod
+    def __valid_password_length(password):
+        length = len(password)
+        if 4 < length < 12:
+            return True
+        else:
+            return False
 
     @staticmethod
     def user_auth(name, password):
@@ -44,12 +52,17 @@ class User:
             print("OSError: Bad file descriptor. Type must be string.")
 
 
-# Tests
-print(User.user_auth('Hans', 'fr6YSKFLd'))
-print(User.user_auth('Hans', 'fr6YSKF'))
-print(User.user_auth('Clampe', 'gU1F1KqG'))
-print(User.user_auth('Danielle', '1234'))
-print(User.user_auth('Eliyahu', 'Rfh320Y'))
-print(User.user_auth('Brame', 'eU5SUpIfTv5'))
-print(User.user_auth('Nedda', 'mc1NeDS75DI6'))
-print(User.user_auth('Cassandry Baskerfield', '49cJqO8CuM'))
+# # Tests
+# print(User.user_auth('Hans', 'fr6YSKFLd'))
+# print(User.user_auth('Hans', 'fr6YSKF'))
+# print(User.user_auth('Clampe', 'gU1F1KqG'))
+# print(User.user_auth('Danielle', '1234'))
+# print(User.user_auth('Eliyahu', 'Rfh320Y'))
+# print(User.user_auth('Brame', 'eU5SUpIfTv5'))
+# print(User.user_auth('Nedda', 'mc1NeDS75DI6'))
+# print(User.user_auth('Cassandry Baskerfield', '49cJqO8CuM'))
+
+# Test for new check password func
+eliyahu = User(34366, 'eliyahu', 'korn', '021693!!')
+eliyahu.password = 'success'
+print(eliyahu.password)
