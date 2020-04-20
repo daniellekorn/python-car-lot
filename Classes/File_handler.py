ad0899df084d0c60ed39c10400544998863b9098
Danielle Korn <daniella.korn@gmail.com>
2020-04-20 18:58:24 +0300

    playing with paths; added to class dec

--- Classes/File_handler.py
+++ Classes/File_handler.py
@@ -1,21 +1,20 @@
 from csv import reader
 from csv import DictReader
 from csv import DictWriter
-import csv
-import os
 from pathlib import Path
 import os
+import csv
 
 
 class FileHandler:
     def __init__(self, file_name=""):
+        main_dir = str(Path(__file__).parent.parent)
         self.file_name = file_name
-        self.cur_dir = os.getcwd()
+        self._csv_path = f"{main_dir}{os.sep}CSV{os.sep}{self.file_name}"
 
-    @staticmethod
-    def load_from_csv(file_name):
+    def load_from_csv(self):
         try:
-            with open(file_name) as file:
+            with open(self._csv_path) as file:
                 csv_reader = DictReader(file)
                 for row in csv_reader:
                     print(row)
@@ -24,16 +23,15 @@ class FileHandler:
         except OSError:
             print("OSError: Bad file descriptor. Type must be string.")
 
-    @staticmethod
-    def append_to_csv(file_name, data):
-        with open(file_name) as file:
+    def append_to_csv(self, data):
+        with open(self._csv_path) as file:
             csv_reader = reader(file)
             file_data = list(csv_reader)
             existing_headers = file_data[0]
             existing_ids = []
             for item in file_data[1:]:
                 existing_ids.append(item[0])
-        with open(file_name, "a") as file:
+        with open(self._csv_path, "a") as file:
             try:
                 csv_writer = DictWriter(file, fieldnames=existing_headers)
                 if "user_id" not in data:
@@ -49,17 +47,17 @@ class FileHandler:
     def data_join(**kwargs):
         pass
 
-    @staticmethod
-    def loop_through_and(func, file_name, user_id):
+    def loop_through_and(self, func, user_id):
         try:
-            with open(file_name) as inp:
+            with open(self._csv_path) as inp:
                 csv_reader = reader(inp)
                 file_data = list(csv_reader)
         except FileNotFoundError:
             print("FileNotFoundError: Please input an appropriate path.")
         else:
-            current_data, updated_data = func(user_id, file_data)
-            with open(file_name, "w") as output:
+            current_data = [row for row in file_data]
+            updated_data = func(user_id, file_data)
+            with open(self._csv_path, "w") as output:
                 writer = csv.writer(output)
                 for item in updated_data:
                     writer.writerow(item)
@@ -68,11 +66,13 @@ class FileHandler:
             else:
                 return False
 
+    @staticmethod
+    def remove_from_csv(user_id, file_data):
+        updated_data = [row for row in file_data if row[0] != user_id]
+        return updated_data
 
-def remove_from_csv(user_id, file_data):
-    current_data = [row for row in file_data]
-    updated_data = [row for row in file_data if row[0] != user_id]
-    return current_data, updated_data
 
+# def update_csv(user_id, row, file_data):
+users = FileHandler("User")
 
-# print(FileHandler.loop_through_and(remove_from_csv, "/Users/daniellekorn/PycharmProjects/car_lot/CSV/User", '5224f411-57aa-4dd2-9cfe-7b76cb92ae3c'))
+# print(FileHandler.loop_through_and(FileHandler.remove_from_csv, "/Users/daniellekorn/PycharmProjects/car_lot/CSV/User", '5224f411-57aa-4dd2-9cfe-7b76cb92ae3c'))
