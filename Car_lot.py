498d7e48d5f14cb1a8f2e04e7f3cc557a79b3180
Danielle Korn <daniella.korn@gmail.com>
2020-04-21 16:32:03 +0300

    ex3 day2 complete: update_salar_by_name

--- Car_lot.py
+++ Car_lot.py
@@ -1,6 +1,6 @@
 from csv import DictReader
 from file_handler import FileHandler
-
+import definitions
 
 class CarLot:
     vehicle_handler = FileHandler("vehicle")
@@ -19,15 +19,28 @@ class CarLot:
 
     # need to return t/f and add error handling
     def update_salary_by_name(self, employee_salary, name):
+        first_name_pos = definitions.file_data.get("user").get("columns").index("first_name")
+        last_name_pos = definitions.file_data.get("user").get("columns").index("last_name")
+        salary_pos = definitions.file_data.get("user").get("columns").index("salary")
+        name = name.lower()
+
         file_data = self.user_handler.load_from_csv()
         all_employees = []
+        found = False
         for row in file_data:
-            if row[2].lower() == name.lower() or f"{row[1]} {row[2]}".lower() == name.lower():
-                new_data = [item if index != 5 else employee_salary for index, item in enumerate(row)]
+            if row[last_name_pos].lower() == name or f"{row[first_name_pos]} {row[last_name_pos]}".lower() == name:
+                found = True
+                new_data = [item if index != salary_pos else employee_salary for index, item in enumerate(row)]
                 all_employees.append(new_data)
             else:
                 all_employees.append(row)
+        # user file handler class to write updated info to csv
         self.user_handler.write_to_csv(all_employees)
+        # checks if employee successfully given raise
+        if found:
+            return True
+        else:
+            return False
 
 
 
@@ -35,4 +48,4 @@ class CarLot:
 lot = CarLot()
 # print(lot.update_salary_by_name(56, 'sam'))
 # print(lot.update_salary_by_name(54, 'dorgon'))
-lot.get_fleet_size("/Users/daniellekorn/Downloads/mock.csv")
+# lot.get_fleet_size("/Users/daniellekorn/Downloads/mock.csv")
